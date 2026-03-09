c490cfbb88df994598cf2fa2478a95e73b78f0c3
Sarvadnya Kalyankar <sarvadnyavk51@gmail.com>
2026-03-10 00:07:31 +0530

    fix ssl connection for tidb

--- backend/database.py
+++ backend/database.py
@@ -5,29 +5,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_USER = os.getenv("DB_USER", "root")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "")
-DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
-DB_PORT = os.getenv("DB_PORT", "3306")
-DB_NAME = os.getenv("DB_NAME", "api_db")
-
-# To handle the case when DB doesn't exist yet, we should ideally not hardcode DB creation.
-# If api_db does not exist, the user must create it first like in the node version.
-SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://2QgbvpdT4isaReH.root:we6eoDydotZAxX9F@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/task_manager?ssl=true"
+# TiDB Cloud connection URL (NO ?ssl=true here)
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://2QgbvpdT4isaReH.root:we6eoDydotZAxX9F@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/task_manager"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
+    pool_recycle=3600,
     connect_args={
-        "ssl": {
-            "ssl": {}
-        }
+        "ssl": {}
     }
 )
 
-# SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_recycle=3600)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
 
