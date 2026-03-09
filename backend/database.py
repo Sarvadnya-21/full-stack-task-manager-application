01d2117f0de1c4a1164d8f36b9b986726b0555fc
Sarvadnya Kalyankar <sarvadnyavk51@gmail.com>
2026-03-10 00:39:53 +0530

    fix ssl connection & updated db url for tidb

--- backend/database.py
+++ backend/database.py
@@ -6,13 +6,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # TiDB Cloud connection URL (NO ?ssl=true here)
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://2QgbvpdT4isaReH.root:we6eoDydotZAxX9F@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/task_manager"
+# SQLALCHEMY_DATABASE_URL = "mysql+pymysql://2QgbvpdT4isaReH.root:we6eoDydotZAxX9F@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/task_manager"
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
+    DATABASE_URL,
     pool_recycle=3600,
     connect_args={
-        "ssl": {}
+        "ssl": {
+            "ssl_verify_cert": True,
+            "ssl_verify_identity": True
+        }
     }
 )
 
