748105501f4fdc077963f0c4a9b7fee225947848
Sarvadnya Kalyankar <sarvadnyavk51@gmail.com>
2026-03-09 23:35:43 +0530

    fix TiDB SSL connection

--- backend/database.py
+++ backend/database.py
@@ -13,8 +13,8 @@ DB_NAME = os.getenv("DB_NAME", "api_db")
 
 # To handle the case when DB doesn't exist yet, we should ideally not hardcode DB creation.
 # If api_db does not exist, the user must create it first like in the node version.
-# SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://2QgbvpdT4isaReH.root:we6eoDydotZAxX9F@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/task_manager?ssl=true"
+# SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_recycle=3600)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
